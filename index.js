7ccb5e4304cd03031244d663152fe0c22da83364
Vladimir Turcan <vladimir.turcan@sumup.com>
2021-03-19 18:45:03 +0200

    add port from env

--- index.js
+++ index.js
@@ -34,6 +34,6 @@ server.use((err, req, res, next) => {
   });
 });
 
-server.listen(5000, () => {
+server.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on port 5000`);
 });
